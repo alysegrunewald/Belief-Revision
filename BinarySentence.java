307922185a680751da1126e8fb8986392d3e7dae
alysegrunewald <50072748+alysegrunewald@users.noreply.github.com>
2019-05-09 23:14:22 +0200

    Update BinarySentence.java

--- BinarySentence.java
+++ BinarySentence.java
@@ -9,14 +9,17 @@ public class BinarySentence extends BeliefADT{
     super();
     // TODO Auto-generated constructor stub
 
-    String[] seperate = string.split("|");
-    SingleLiteralSentence l = new SingleLiteralSentence(seperate[0]);
-    this.notFirstLiteral = l.getNotLiteral();
-    this.firstLiteral = l.getLiteral();
+    String[] seperate = string.split("\\|");
+//    for (String s : seperate) {
+//      System.out.println("***" + s);
+//    }
+    SingleLiteralSentence s1 = new SingleLiteralSentence(seperate[0]);
+    this.notFirstLiteral = s1.getNotLiteral();
+    this.firstLiteral = s1.getLiteral();
     
-    SingleLiteralSentence s = new SingleLiteralSentence(seperate[1]);
-    this.notFirstLiteral = s.getNotLiteral();
-    this.firstLiteral = s.getLiteral();
+    SingleLiteralSentence s2 = new SingleLiteralSentence(seperate[1]);
+    this.notSecondLiteral = s2.getNotLiteral();
+    this.secondLiteral = s2.getLiteral();
   }
   
   public boolean getNotFirstLiteral() {
