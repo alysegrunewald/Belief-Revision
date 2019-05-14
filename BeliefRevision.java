a40a4f9584099709c28ea227c2b5269bd5a4a89b
alysegrunewald <50072748+alysegrunewald@users.noreply.github.com>
2019-05-14 23:26:18 +0200

    Update BeliefRevision.java

--- BeliefRevision.java
+++ BeliefRevision.java
@@ -1,4 +1,25 @@
 
+
 public class BeliefRevision {
+  
+  //Epistemic Entrenchment
+  //Premises:
+  //1. if p logically entails q, give up p and retain q (i.e. higher rank for q)
+  //2. p&q has higher entrenchment than p alone and q alone
+  //3. sentences already not in B have minimal epistemic entrenchment in relation to B
+  //4.  only logically valid sentences can be maximal in ranking
+  
+  public BeliefBaseADT expansion(BeliefBaseADT beliefBase) {
+    return null;
+  }
+  
+  public BeliefBaseADT revision(BeliefBaseADT beliefBase) {
+    return null;
+  }
+  
+  public BeliefBaseADT contraction(BeliefBaseADT beliefBase) {
+    return null;
+  }
 
 }
+
