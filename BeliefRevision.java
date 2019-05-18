6165d71becb6fdb0e12e84faaa245dbbc1d66d49
hallesterling <50139654+hallesterling@users.noreply.github.com>
2019-05-18 06:54:05 -0500

    Update BeliefRevision.java

--- BeliefRevision.java
+++ BeliefRevision.java
@@ -26,20 +26,55 @@ public class BeliefRevision {
       return beliefBase;
   }
   
-  public void rank(ArrayList<BeliefADT> beliefBase) {   
+  public void rank(BeliefBaseADT beliefBase) {   
+       ArrayList<BeliefADT> beliefs = beliefBase.getBeliefBase();
+       String[][] truthTable = beliefBase.truthTable(null);
        int i = 0;
-       for (i = 0; i < beliefBase.size(); i++) {
-           beliefBase.get(i).setRank(i + 1);
+       for (i = 0; i < beliefs.size(); i++) {
+           beliefs.get(i).setRank(i + 1);
        }
        
-       for (i = 0; i < beliefBase.size() - 1; i++) {
-           if (entails(beliefBase.get(i), beliefBase.get(i + 1)) == true) {
-               beliefBase.get(i).setRank(beliefBase.get(i + 1).getRank() + 1);
+       for (i = 0; i < beliefs.size() - 1; i++) {
+           if (entails(beliefs.get(i), beliefs.get(i + 1), truthTable) == true) {
+               beliefs.get(i).setRank(beliefs.get(i + 1).getRank() + 1);
            }
        }
   }
   
-  public boolean entails(BeliefADT belief1, BeliefADT belief2, tring[][]) {
+  public boolean entails(BeliefADT belief1, BeliefADT belief2, String[][] truthTable) {
+      boolean entails = false;
+      int i = 0;
+      int belief1index = 0;
+      int belief2index = 0;
       
-      return true;
+      for (i = 0; i < truthTable.length; i++) {
+          if (belief1.toString().equals(truthTable[i][0])) {
+              belief1index = i;
+          }
+          if (belief2.toString().equals(truthTable[i][0])) {
+              belief2index = i;
+          }
+      }
+      
+      int counter = 0;
+         
+      for (i = 0; i < truthTable[0].length; i++) {
+          if (truthTable[belief1index][i].equals("1")) {
+              counter++;
+          }
+      }
+      
+      int counter2 = 0;
+      for (i = 0; i < truthTable[0].length; i++) {
+          if (truthTable[belief1index][i].equals("1") && truthTable[belief2index][i].equals("1")) {
+              counter2++;
+          }
+      }
+      
+      if(counter == counter2) {
+          entails = true;
+      }
+      
+      return entails;
   }
+}
