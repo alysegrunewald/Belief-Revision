47c5ef510a4e0d07c80e37e6ff4aacbaadbcdb17
alysegrunewald <50072748+alysegrunewald@users.noreply.github.com>
2019-05-18 13:56:47 +0200

    Update BeliefADT.java

--- BeliefADT.java
+++ BeliefADT.java
@@ -1,7 +1,20 @@
 
+
 public class BeliefADT {
   
+  private int rank;
+  
   public BeliefADT () {
-    
+    this.rank=0;
+  }
+  
+  public int getRank() {
+    return this.rank;
+  }
+  
+  public void setRank(int rank) {
+    this.rank = rank;
+    return;
   }
 }
+
