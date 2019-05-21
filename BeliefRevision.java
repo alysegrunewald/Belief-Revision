2219c76921f1f7a0f26c546f1ce6e245e49fae04
alysegrunewald <50072748+alysegrunewald@users.noreply.github.com>
2019-05-21 15:09:16 +0200

    Update BeliefRevision.java

--- BeliefRevision.java
+++ BeliefRevision.java
@@ -9,53 +9,71 @@ public class BeliefRevision {
   //3. sentences already not in B have minimal epistemic entrenchment in relation to B
   //4.  only logically valid sentences can be maximal in ranking
   
-  public void rank(BeliefBaseADT beliefBase) {
-    ArrayList<BeliefADT> beliefs = beliefBase.getBeliefBase();
-    String[][] truthTable = beliefBase.truthTable(null);
+//  public void rank(BeliefBaseADT beliefBase) {
+//    ArrayList<BeliefADT> beliefs = beliefBase.getBeliefBase();
+//    String[][] truthTable = beliefBase.truthTable(null);
+//    
+////    System.out.println(beliefBase.toString());
+//
+//    for (int i = 0; i < beliefs.size(); i++) {
+//      beliefs.get(i).setRank(0);
+//    }
+//
+//    for (int i = 0; i < beliefs.size(); i++) {
+//      for(int j = 0; j < beliefs.size(); j++) {
+//        if(i != j) {
+//          if (entails(beliefs.get(i), beliefs.get(j), truthTable) == true) {
+//            beliefs.get(j).setRank(beliefs.get(j).getRank() + 1);
+//          }
+//        }
+//      }
+//    }
+//  }
+  
+  public void selection(ArrayList<String> remainderSet) {
+    //Maxichoice or full meet contraction
+    //Arbitrary selection of removal, no intersection of remainder set
+  }
+  
+  public ArrayList<ArrayList<String>> remainderSet(BeliefBaseADT beliefBase, BeliefADT newBelief) {
+    String[][] truthTable = beliefBase.truthTable(newBelief);
     
-//    System.out.println(beliefBase.toString());
-
-    for (int i = 0; i < beliefs.size(); i++) {
-      beliefs.get(i).setRank(0);
-    }
-
-    for (int i = 0; i < beliefs.size(); i++) {
-      for(int j = 0; j < beliefs.size(); j++) {
-        if(i != j) {
-          if (entails(beliefs.get(i), beliefs.get(j), truthTable) == true) {
-            beliefs.get(j).setRank(beliefs.get(j).getRank() + 1);
-          }
-        }
-      }
-    }
+    //Go through belief base, find all combiniations of what is in the belief base
+    
+    //Find inclusion maximal combos of where belief base does not entail new belief
+    //Every subset of B that does not entail phi will be added to remainder set
+    
+    //Find a way to ensure remainder set only includes largest subsets of B (inclusion maximal)
+    
+    return null;
   }
 
-  public boolean entails(BeliefADT belief1, BeliefADT belief2, String[][] truthTable) {
+  public boolean entails(BeliefADT belief1, BeliefADT notPhi, String[][] truthTable) {
 
     boolean entails = false;
     int i = 0;
     int belief1index = 0;
-    int belief2index = 0;
+    int notPhiIndex = 0;
     
     for (i = 0; i < truthTable.length; i++) {
         if (belief1.toString().equals(truthTable[i][0])) {
             belief1index = i;
         }
-        if (belief2.toString().equals(truthTable[i][0])) {
-            belief2index = i;
+        if (notPhi.toString().equals(truthTable[i][0])) {
+            notPhiIndex = i;
         }
     }
     
     int counter = 0;  
-    for (i = 0; i < truthTable[0].length; i++) {
+    for (i = 1; i < truthTable[0].length; i++) {
         if (truthTable[belief1index][i].equals("1")) {
             counter++;
         }
     }
 
     int counter2 = 0;
-    for (i = 0; i < truthTable[0].length; i++) {
-        if (truthTable[belief1index][i].equals("1") && truthTable[belief2index][i].equals("1")) {
+    for (i = 1; i < truthTable[0].length; i++) {
+        if (truthTable[notPhiIndex][i].equals("0")) {
             counter2++;
         }
     }
@@ -66,70 +84,23 @@ public class BeliefRevision {
     return entails;
 }
   
-  public ArrayList<BeliefADT> expansion(BeliefBaseADT beliefBase) {
-    return null;
-  }
-  
-  public ArrayList<BeliefADT> revision(BeliefBaseADT beliefBase) {
-    return null;
-  }
-  
-  public ArrayList<BeliefADT> contraction(BeliefBaseADT beliefBase, BeliefADT newBelief) {
+  public ArrayList<BeliefADT> expansion(BeliefBaseADT<BeliefADT> beliefBase, BeliefADT belief) {
     ArrayList<BeliefADT> beliefs = beliefBase.getBeliefBase();
-    String[][] truthTable = beliefBase.truthTable(newBelief);
-    int index = (int)Math.round(Math.log10(truthTable[0].length-1)/Math.log10(2));
     
-    ArrayList<String> contradictions = new ArrayList<String>();
+    if(!beliefs.contains(belief)) {
+      beliefs.add(belief);
+    }  
     
-    //Rank of current beliefs
-    rank(beliefBase);
+    return beliefs;
+  }
+  
+  public ArrayList<BeliefADT> contraction(BeliefBaseADT<BeliefADT> beliefBase, BeliefADT belief) {
+    ArrayList<BeliefADT> beliefs = beliefBase.getBeliefBase();
     
-    //Find contradictions in belief base from new new belief
-    for(int i = 0; i < truthTable[0].length; i++) {
-      if (truthTable[truthTable.length-1][i].equals("1")) {
-        for (int j = index; j < truthTable.length-1; j++) {
-          if (truthTable[j][i].equals("0") && !contradictions.contains(truthTable[j][0])) {
-            contradictions.add(truthTable[j][0]);
-          }
-        }
-      }
-    }
-    System.out.println(contradictions);
-    //convert contradictions String to contradictions of type BeliefADT
-    ArrayList<BeliefADT> contradictions2 = new ArrayList<BeliefADT>();
-    for (int i = 0; i < contradictions.size(); i++) {
-        for (int j = 0; j < beliefs.size(); j++) {
-            if (contradictions.get(i).equals(beliefs.get(j).toString())) {
-                contradictions2.add(beliefs.get(j));
-            }
-        }
-    }
+    if(beliefs.contains(belief)) {
+      beliefs.add(belief);
+    }  
     
-    //Of the contradicting beliefs, find lowest rank
-    BeliefADT minRank = contradictions2.get(0);
-    for (int i = 0; i < contradictions2.size(); i++) {
-        if (contradictions2.get(i).getRank() < minRank.getRank()) {
-            minRank = contradictions2.get(i);
-        }
-        else if (contradictions2.get(i).getRank() == minRank.getRank()) {
-            int b1 = 0;
-            int b2 = 0;
-            for (int j = 0; j < beliefs.size(); j++) {
-                if (minRank.toString().equals(beliefs.get(j).toString())) {
-                    b1 = j;
-                }
-                if (contradictions2.get(i).toString().equals(beliefs.get(j).toString())) {
-                    b2 = j;
-                }
-            }
-            if (b2 < b1) {
-                minRank = contradictions2.get(i); 
-            }
-        }
-    }
- 
-    beliefs.remove(minRank);
-
     return beliefs;
   }
 }
